39342f3ea02487fc46363edc2b435bffd13e1a10
DeliciousBread481 <3574467868@qq.com>
2026-07-16 18:53:48 +0800

    Fix build

--- src/main/java/deliciousbread481/combatdebug/core/CombatDebugCore.java
+++ src/main/java/deliciousbread481/combatdebug/core/CombatDebugCore.java
@@ -1,19 +1,17 @@
 package deliciousbread481.combatdebug.core;
 
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
-import zone.rong.mixinbooter.IEarlyMixinLoader;
+import org.spongepowered.asm.mixin.Mixins;
 
 import javax.annotation.Nullable;
-import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
 @IFMLLoadingPlugin.MCVersion("1.12.2")
 @IFMLLoadingPlugin.Name("CombatDebugCore")
-public class CombatDebugCore implements IFMLLoadingPlugin, IEarlyMixinLoader {
+public class CombatDebugCore implements IFMLLoadingPlugin {
 
-    @Override public List<String> getMixinConfigs() {
-        return Collections.singletonList("mixins.combatdebug.json");
+    public CombatDebugCore() {
+        Mixins.addConfigurations("mixins.combatdebug.json");
     }
 
     @Override public String[] getASMTransformerClass() { return new String[0]; }
